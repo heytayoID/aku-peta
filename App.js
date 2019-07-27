d9d5799bfbb4e09950668f0df874cdfb506a50b6
Aden Trisna Daud Kurnia <adentrisnadaudkurnia@gmail.com>
2019-07-28 04:25:18 +0700

    adding pin location, text coordinate, fix UI

--- App.js
+++ App.js
@@ -1,114 +1,137 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
+import React,{Component} from 'react'
+import {View, Text, TextInput, StyleSheet,Alert} from 'react-native'
+import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps'
 
-import React, {Fragment} from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+let id = 0
+class App extends Component {
+  state = {
+    region : {
+      latitude: -6.3164643825140985,
+      longitude: 106.6482881270349,
+      latitudeDelta: 0.0004288809190189369,
+      longitudeDelta: 0.0004315003752708435,
+    },
+    marker : {
+      coordinate : {   
+        longitude: 106.64824672043324,
+        latitude: -6.316456218159012
+      },
+      name : "Meteor Inovasi Digital .PT"
+    },
+  }
+  randomColor = () => {
+    return `#${Math.floor(Math.random() * 16777215)
+      .toString(16)
+      .padStart(6, 0)}`;
+  }
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+  handleMapPress = (data) => {
+    this.setState({
+      marker : {
+        coordinate: data.nativeEvent.coordinate,
+        name : data.nativeEvent.name
+      },
+      region : {
+        latitude: data.nativeEvent.coordinate.latitude,
+        longitude: data.nativeEvent.coordinate.longitude,
+        latitudeDelta: this.state.region.latitudeDelta,
+        longitudeDelta: this.state.region.longitudeDelta,
+      }
+    })
+  }
 
-const App = () => {
-  return (
-    <Fragment>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </Fragment>
-  );
-};
+  handleRegionChange = (region) => {
+    this.setState({
+      region : region
+    })
+  }
+
+  render() {
+    return (
+        <View style={styles.container}>
+            <MapView
+               provider={PROVIDER_GOOGLE}
+               style={styles.map}
+               initialRegion={this.state.region}
+               onLongPress={data => this.handleMapPress(data)}
+               onPoiClick={data => this.handleMapPress(data)}
+               onRegionChangeComplete={region => this.handleRegionChange(region)}
+            >
+              <Marker
+                title={this.state.marker.name}
+                coordinate={this.state.marker.coordinate}
+              />
+           </MapView>
+
+             <View style={[styles.searchBarWrapper]}>
+              <TextInput placeholder={"Temukan ..."} placeholderTextColor={"#aaa"} style={styles.searchBar}/>
+             </View>
+
+             <View style={[styles.bubble, styles.latlng]}>
+                <Text style={styles.centeredText}>
+                  {this.state.region.latitude.toPrecision(7)},
+                  {this.state.region.longitude.toPrecision(7)}
+                </Text>
+              </View>
+
+         </View>
+      )
+  }
+}
+
+export default App
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
+    container: {
+        ...StyleSheet.absoluteFillObject,
+        justifyContent: 'flex-end',
+        alignItems: 'center',
+    },
+    map: {
+        ...StyleSheet.absoluteFillObject,
+    },
+    bubble: {
+        backgroundColor: 'rgba(255,255,255,0.7)',
+        paddingHorizontal: 18,
+        paddingVertical: 12,
+        borderRadius: 20,
+        bottom:20
+    },
+    searchBarWrapper : {
+      backgroundColor: 'rgba(255,255,255,1)',
+      position: 'absolute',
+      height : 50,
+      margin:10,
+      padding:"auto",
+      borderRadius:5,
+      borderWidth:.3,
+      top : 0,
+      right: 0,
+      left : 0,
+    },
+    searchBar : {
+      fontSize: 18,
+    },
+    latlng: {
+        width: 200,
+        alignItems: 'stretch',
+    },
+    button: {
+        width: 100,
+        paddingHorizontal: 8,
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginHorizontal: 5,
+    },
+    buttonContainer: {
+        flexDirection: 'row',
+        marginVertical: 20,
+        backgroundColor: 'transparent',
+    },
+    buttonText: {
+        textAlign: 'center',
+    },
+    centeredText: {
+        textAlign: 'center'
+    },
 });
-
-export default App;
